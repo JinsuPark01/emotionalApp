a55bf67ab06ecc532e2fc5f896c5fe0d0132d4d6
gary8 <gary7345@hs.ac.kr>
2025-08-04 08:55:59 +0900

    .

--- app/src/main/java/com/example/emotionalapp/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/example/emotionalapp/ui/chat/ChatActivity.kt
@@ -19,15 +19,30 @@ class ChatActivity : BottomNavActivity() {
         setupBottomNavigation()
 
         // 카드 뷰 내부의 content_layout 클릭 이벤트 연결
-        val call = findViewById<LinearLayout>(R.id.content_layout1)
+        val mail = findViewById<LinearLayout>(R.id.content_layout1)
         val agentChat = findViewById<LinearLayout>(R.id.content_layout2)
 
-        call.setOnClickListener {
-            // 전화 연결 (전화 앱으로 이동)
-            val intent = Intent(Intent.ACTION_DIAL)
-            intent.data = Uri.parse("tel:119")
-            startActivity(intent)
+        mail.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SEND).apply {
+                type = "message/rfc822" // 이메일 전용 MIME 타입
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("support@example.com"))
+                putExtra(Intent.EXTRA_SUBJECT, "문의 사항")
+                putExtra(Intent.EXTRA_TEXT, "여기에 내용을 입력하세요.")
+            }
+
+            try {
+                startActivity(Intent.createChooser(emailIntent, "이메일 앱을 선택하세요"))
+            } catch (e: Exception) {
+                Toast.makeText(this, "메일 앱이 설치되어 있지 않습니다.", Toast.LENGTH_SHORT).show()
+            }
         }
+//        call.setOnClickListener {
+//            // 전화 연결 (전화 앱으로 이동)
+//            val intent = Intent(Intent.ACTION_DIAL)
+//            intent.data = Uri.parse("tel:119")
+//            startActivity(intent)
+//            finish()
+//        }
         agentChat.setOnClickListener {
             // 전화 연결 (전화 앱으로 이동)
             Toast.makeText(this, "미구현 기능입니다.", Toast.LENGTH_SHORT).show()
